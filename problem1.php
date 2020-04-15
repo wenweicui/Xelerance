9f751d3a0e57605c5eff72d7bff657599b8abb63
WillC <wenweicui@gmail.com>
2020-04-15 16:43:07 -0400

    Update problem1.php

--- problem1.php
+++ problem1.php
@@ -1,16 +1,13 @@
 <?php
 
-  function sumOfOddFab($max) {
-    $ele1 = 0;
-    $ele2 = 1;
-    $sum = 1;
-    while($ele1 + $ele2 <= $max) {
-      $current = $ele1 + $ele2;
+  function sumOfOddFab($max, $init = 0, $add = 1) {
+    $sum = $init + $add;
+    while(($current = $init + $add) <= $max) {
       if ($current%2 == 1) {
         $sum += $current;
       }
-      $ele1 = $ele2;
-      $ele2 = $current;
+      $init = $add;
+      $add = $current;
     }
     return $sum;
   }
